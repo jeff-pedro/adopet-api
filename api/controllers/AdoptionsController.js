ddb3a4d913d504d1882bc2d0b3b0ead224580d35
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2023-04-14 13:50:28 -0300

    add router /pets/{id}/adoption to create a new adoption

---
+++ api/controllers/AdoptionsController.js
@@ -0,0 +1,26 @@
+const database = require('../models')
+
+class AdoptionsController {
+  static async createAdoption(req, res) {
+    const { body } = req
+    const petId = req.params.id
+
+    const adoptionData = {
+      animal: Number(petId),
+      tutor: Number(body.tutor),
+      date: body.date || new Date()
+    }
+
+    try {
+      const adoption = await database.Adoption.create(adoptionData)
+      await database.Pet.update({ status: 'Adopted' }, { where: { id: Number(petId) } })
+      return res.status(200).json({
+        adoption: adoption
+      })
+    } catch (err) {
+      return res.status(500).json({ error: err.message })
+    }
+  }
+}
+
+module.exports = AdoptionsController
--- api/routes/petsRoute.js
+++ api/routes/petsRoute.js
@@ -1,9 +1,11 @@
 const { Router } = require('express')
 const PetsController = require('../controllers/PetsController.js')
+const AdoptionsController = require('../controllers/AdoptionsController.js')
 const routes = Router()
 
 routes
   .post('/pets', PetsController.createPet)
+  .post('/pets/:id/adoption', AdoptionsController.createAdoption)
   .get('/pets', PetsController.getAllPets)
   .get('/pets/:id', PetsController.getOnePet)
   .put('/pets/:id', PetsController.updateManyPetProperties)
