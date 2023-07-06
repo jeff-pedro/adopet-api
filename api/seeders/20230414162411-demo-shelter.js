dba7ae437b42e07ae78008d6014f4dfb25d7a3c8
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2023-07-06 19:12:47 -0300

    add more demo data in shelters seed

--- api/seeders/20230414162411-demo-shelter.js
+++ api/seeders/20230414162411-demo-shelter.js
@@ -4,17 +4,41 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Shelters', [{
-      name: 'Shelter Rúbeo Hagrid',
+      name: 'Rúbeo Hagrid Shelter',
       email: 'shelter_hagrid@mail.com',
       phone: '+77666999777',
       city: 'Dufftown',
       state: 'Moray',
       createdAt: new Date(),
       updatedAt: new Date()
+    }, {
+      name: 'Happy Pets Shelter',
+      email: 'happyshelter@mail.com',
+      phone: '+24777444777',
+      city: 'Bergen',
+      state: 'Vestland',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      name: 'Andry Birds Shelter',
+      email: 'angryshelter@mail.com',
+      phone: '+44555999555',
+      city: 'Bird Island',
+      state: 'Pacific Ocean',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      name: 'Gotham City Shelter for Cats and Dogs',
+      email: 'gothamshelter@mail.com',
+      phone: '+1999111999',
+      city: 'Gotham',
+      state: 'New York',
+      createdAt: new Date(),
+      updatedAt: new Date()
     }], {})
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Shelters', null, {});
+    await queryInterface.bulkDelete('Shelters', null, {})
   }
 }
