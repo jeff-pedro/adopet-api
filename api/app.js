33b8df8b6ebd8261dc74c8ffd2fa72b1eb1b0e74
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2023-08-04 10:54:24 -0300

    setup redis, express session and fix code

--- api/app.js
+++ api/app.js
@@ -1,16 +1,57 @@
 const express = require('express')
+const session = require('express-session')
+const { createClient } = require('redis')
+const RedisStore = require('connect-redis').default
+const cors = require('cors')
 
 const router = require('./routes')
-const pagination = require('./middlewares/pagination.js')
-const auth = require('./middlewares/authentication.js')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors()) //inseri isso para  não ter conflito nas requisuições
 
-app.use('/login', auth)
+// Configure redis client
+let redisClient = createClient()
+
+redisClient.connect()
+  .then(() => {
+    console.log('Connected to redis successfully.')
+  })
+  .catch(err => {
+    console.log('Could not establish a connection with redis. ', err)
+  })
+
+// Configure redis store
+let redisStore = new RedisStore({
+  client: redisClient,
+  prefix: 'adopet:',
+})
+
+// Configure session middleware
+app.use(session({
+  store: redisStore,
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  // cookie: {
+  //   httpOnly: true,
+  //   maxAge: 3600
+  // }
+}))
+
 router(app)
 
-app.use(pagination)
+app.use((req, res, next) => {
+  console.log(req.session)
+  next()
+})
+
+// Error handler middleware
+app.use((err, req, res, next) => {
+  if(err) {
+    res.json({ error: err })
+  }
+})
 
 module.exports = app
