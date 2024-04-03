8b3621835aa4d9ec67c0fd8078e09598eb048205
Jefferson Pedro <jeffersonpedro89@gmail.com>
2024-04-03 18:39:50 -0300

    Apply linter

--- api/migrations/20240402164329-create-permission.js
+++ api/migrations/20240402164329-create-permission.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -23,9 +23,9 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    })
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Permissions');
+    await queryInterface.dropTable('Permissions')
   }
-};
+}
