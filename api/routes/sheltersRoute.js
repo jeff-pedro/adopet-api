dad5ae4db043fecf4e6f59ea59d293681d1f0491
Jefferson Pedro <jeffersonpedro89@gmail.com>
2024-05-02 10:29:50 -0300

    REFACTOR: refactor services, controllers and route

--- api/routes/sheltersRoute.js
+++ api/routes/sheltersRoute.js
@@ -3,16 +3,18 @@ const SheltersController = require('../controllers/SheltersController.js')
 const pagination = require('../middlewares/pagination.js')
 const authorization = require('../middlewares/authorization.js')
 
+const sheltersController = new SheltersController()
+
 const routes = Router()
 
 routes.use(authorization)
 
 routes
-  .post('/shelters', SheltersController.createShelter)
-  .get('/shelters', SheltersController.getAllShelters, pagination)
-  .get('/shelters/:id', SheltersController.getOneShelter)
-  .put('/shelters/:id', SheltersController.updateManyShelterProperties)
-  .patch('/shelters/:id', SheltersController.updateOneShelterProperty)
-  .delete('/shelters/:id', SheltersController.deleteShelter)
+  .post('/shelters', (req, res, next) => sheltersController.createNew(req, res, next))
+  .get('/shelters', (req, res, next) => sheltersController.getAll(req, res, next), pagination)
+  .get('/shelters/:id', (req, res, next) => sheltersController.getById(req, res, next))
+  .put('/shelters/:id', (req, res, next) => sheltersController.updateMany(req, res, next))
+  .patch('/shelters/:id', (req, res, next) => sheltersController.updateOne(req, res, next))
+  .delete('/shelters/:id', (req, res, next) => sheltersController.delete(req, res, next))
 
 module.exports = routes
