441c248afa4b275d29b9bba5e5a02f05a363c3ec
Jefferson Pedro <jeffersonpedro89@gmail.com>
2024-05-02 19:15:42 -0300

    REFACTOR: services, controllers and Profiles routes

--- api/routes/profilesRoute.js
+++ api/routes/profilesRoute.js
@@ -1,12 +1,14 @@
-const { Router } = require('express')
-const routes = Router()
-const ProfilesController = require('../controllers/ProfilesController')
+const routes = require('express').Router()
+const ProfilesController = require('../controllers/ProfilesController.js')
+const pagination = require('../middlewares/pagination.js')
+
+const profilesController = new ProfilesController()
 
 routes
-  .post('/profiles', ProfilesController.createProfile)
-  .get('/profiles', ProfilesController.getAllProfiles)
-  .get('/profiles/:id', ProfilesController.getOneProfile)
-  .put('/profiles/:id', ProfilesController.updateProfile)
-  .delete('/profiles/:id', ProfilesController.deleteProfile)
+  .post('/profiles', (req, res, next) => profilesController.createNew(req, res, next))
+  .get('/profiles', (req, res, next) => profilesController.getAll(req, res, next), pagination)
+  .get('/profiles/:id', (req, res, next) => profilesController.getById(req, res, next))
+  .put('/profiles/:id', (req, res, next) => profilesController.updateMany(req, res, next))
+  .delete('/profiles/:id', (req, res, next) => profilesController.delete(req, res, next))
 
 module.exports = routes
