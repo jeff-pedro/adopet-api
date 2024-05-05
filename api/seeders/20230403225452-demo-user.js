a869b2674e39f3599d1bf2a4b1e8dbf4d82dfe65
Jefferson Pedro <jeffersonpedro89@gmail.com>
2024-05-05 15:12:47 -0300

    REFACTOR: fix seeders to users, shelters and pets

--- api/seeders/20230403225452-demo-user.js
+++ api/seeders/20230403225452-demo-user.js
@@ -1,12 +1,18 @@
 'use strict'
 
+const { genSaltSync, hashSync } = require('bcrypt')
+
+const salt = genSaltSync()
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Users', [{
+    await queryInterface.bulkInsert('users', [{
+      id: '110e23a2-88ab-4fcb-ad97-2660c4c00391',
       name: 'John Wick',
       email: 'john.wick@mail.com',
-      password: 'john123',
+      password: hashSync('john123', salt),
+      salt: genSaltSync(),
       phone: '+711233334444',
       city: 'Padhorje',
       about: 'I am a loving tutor',
@@ -16,9 +22,11 @@ module.exports = {
       updatedAt: new Date()
     },
     {
+      id: '44b91718-e057-4c11-8f90-f53777555f46',
       name: 'Dick Dastardly',
       email: 'dick.dastardly@mail.com',
-      password: 'dick123',
+      password: hashSync('dick123', salt),
+      salt: genSaltSync(),
       phone: '+188244442222',
       city: 'Los Angeles',
       about: 'I am a attencious tutor',
@@ -28,9 +36,11 @@ module.exports = {
       updatedAt: new Date()
     },
     {
+      id: '15d319fc-dc5b-4267-acb7-b10a741953c4',
       name: 'Newt Scamander',
       email: 'newt.scamander@mail.com',
-      password: 'newt123',
+      password: hashSync('newt123', salt),
+      salt: genSaltSync(),
       phone: '+4466211115555',
       city: 'London',
       about: 'I am a caring tutor',
@@ -40,9 +50,11 @@ module.exports = {
       updatedAt: new Date()
     },
     {
+      id: 'b53d4c86-a75b-4550-bf17-e83c688631ac',
       name: 'Hector Barbosa',
       email: 'barbosa@pirates.sea',
-      password: 'hector123',
+      password: hashSync('hector123', salt),
+      salt: genSaltSync(),
       phone: '+011233334444',
       city: 'Lisbon',
       about: 'All pets loves me',
@@ -56,7 +68,7 @@ module.exports = {
 
   async down(queryInterface, Sequelize) {
 
-    await queryInterface.bulkDelete('Users', null, {})
+    await queryInterface.bulkDelete('users', null, {})
 
   }
 }
--- api/seeders/20230414162431-demo-pet.js
+++ api/seeders/20230414162431-demo-pet.js
@@ -1,10 +1,13 @@
 'use strict'
 
+const { v4: uuid } = require('uuid')
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Pets', [{
-      shelter_id: 1,
+    await queryInterface.bulkInsert('pets', [{
+      id: '7c31941f-7918-4ac5-8d25-1f14dc706625',
+      shelter_id: '77c06537-8d57-43fd-8837-24c1d5bbc5a1',
       name: 'Nala',
       birthday: '2023-01-01',
       size: 'Small',
@@ -14,8 +17,10 @@ module.exports = {
       profilePictureUrl: '../../public/Felicia.png',
       createdAt: new Date(),
       updatedAt: new Date()
-    }, {
-      shelter_id: 1,
+    }, 
+    {
+      id: 'ffa8fe79-369f-4c41-804a-ab558673e09b',
+      shelter_id: '77c06537-8d57-43fd-8837-24c1d5bbc5a1',
       name: 'Pretinha',
       birthday: '2023-01-01',
       size: 'Medium',
@@ -25,8 +30,10 @@ module.exports = {
       profilePictureUrl: '../../public/Pretinha.png',
       createdAt: new Date(),
       updatedAt: new Date()
-    }, {
-      shelter_id: 1,
+    }, 
+    {
+      id: '2b880444-79d5-492c-a062-d3f0e622d995',
+      shelter_id: '77c06537-8d57-43fd-8837-24c1d5bbc5a1',
       name: 'Felicia',
       birthday: '2023-01-01',
       size: 'Small',
@@ -36,8 +43,10 @@ module.exports = {
       profilePictureUrl: '../../public/Felicia.png',
       createdAt: new Date(),
       updatedAt: new Date()
-    }, {
-      shelter_id: 1,
+    }, 
+    {
+      id: 'c3147608-c6ff-4ffb-a148-a551426b2236',
+      shelter_id: '77c06537-8d57-43fd-8837-24c1d5bbc5a1',
       name: 'Chewbacca',
       birthday: '2023-01-01',
       size: 'Medium',
@@ -47,8 +56,10 @@ module.exports = {
       profilePictureUrl: '../../public/Chewbacca.png',
       createdAt: new Date(),
       updatedAt: new Date()
-    }, {
-      shelter_id: 1,
+    }, 
+    {
+      id: '8897e34b-558e-4f1a-a172-fdfc486c9f0d',
+      shelter_id: '77c06537-8d57-43fd-8837-24c1d5bbc5a1',
       name: 'Fiona',
       birthday: '2023-01-01',
       size: 'Mini',
@@ -58,8 +69,10 @@ module.exports = {
       profilePictureUrl: '../../public/Fiona.png',
       createdAt: new Date(),
       updatedAt: new Date()
-    }, {
-      shelter_id: 1,
+    }, 
+    {
+      id: '6cf393d0-afcb-4b5a-b9e9-ce843b796d38',
+      shelter_id: '77c06537-8d57-43fd-8837-24c1d5bbc5a1',
       name: 'Yoda',
       birthday: '2023-01-01',
       size: 'Medium',
@@ -70,7 +83,8 @@ module.exports = {
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
-      shelter_id: 1,
+      id: 'adc63fe9-19c4-4189-8835-109932fee223',
+      shelter_id: '77c06537-8d57-43fd-8837-24c1d5bbc5a1',
       name: 'Dementador',
       birthday: '2023-01-01',
       size: 'Big',
@@ -80,8 +94,10 @@ module.exports = {
       profilePictureUrl: '../../public/Dementador.png',
       createdAt: new Date(),
       updatedAt: new Date()
-    }, {
-      shelter_id: 1,
+    }, 
+    {
+      id: '026d37aa-ffc4-47cb-9317-977aac0f92cf',
+      shelter_id: '77c06537-8d57-43fd-8837-24c1d5bbc5a1',
       name: 'Penelope',
       birthday: '2023-01-01',
       size: 'Small',
@@ -91,8 +107,10 @@ module.exports = {
       profilePictureUrl: '../../public/Penelope.png',
       createdAt: new Date(),
       updatedAt: new Date()
-    }, {
-      shelter_id: 1,
+    }, 
+    {
+      id: '626a685f-ea4e-4394-aba8-91efb93542f6',
+      shelter_id: '77c06537-8d57-43fd-8837-24c1d5bbc5a1',
       name: 'Lampeão',
       birthday: '2023-01-01',
       size: 'Small',
@@ -102,8 +120,10 @@ module.exports = {
       profilePictureUrl: '../../public/Lampeao.png',
       createdAt: new Date(),
       updatedAt: new Date()
-    }, {
-      shelter_id: 1,
+    }, 
+    {
+      id: 'b8563ccd-25a1-41e3-8033-840d6a431f0e',
+      shelter_id: '77c06537-8d57-43fd-8837-24c1d5bbc5a1',
       name: 'Kitty',
       birthday: '2023-01-01',
       size: 'Mini',
@@ -113,8 +133,10 @@ module.exports = {
       profilePictureUrl: '../../public/Kitty.png',
       createdAt: new Date(),
       updatedAt: new Date()
-    }, {
-      shelter_id: 1,
+    }, 
+    {
+      id: '10fe91f6-1067-49d7-821e-99e1c9838967',
+      shelter_id: '77c06537-8d57-43fd-8837-24c1d5bbc5a1',
       name: 'Zelda',
       birthday: '2023-01-01',
       size: 'Mini',
@@ -128,6 +150,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Pets', null, {})
+    await queryInterface.bulkDelete('pets', null, {})
   }
 }
