0419416636115c94a7eef8b17001fecf29838ae3
Jefferson Pedro <jeffersonpedro89@gmail.com>
2024-05-14 12:08:09 -0300

    REFACTOR: refactor user login function

--- api/__test__/helper/userLogin.js
+++ api/__test__/helper/userLogin.js
@@ -1,45 +1,17 @@
-const database = require('../../models')
+const { createRandomUsers } = require('../helper/seeders.js')
 
-const { UserService } = require('../../services')
-const userService = new UserService()
+module.exports = async (auth, request, app) => {
 
-async function createUser() {
-  try {
-    const user = await database.User.findOne({
-      where: {
-        email: 'tuner@pirates.sea'
-      }
-    })
-    
-    if (!user) {
-      await userService.createRecord({
-        name: 'Will Turner',
-        email: 'tuner@pirates.sea',
-        password: 'tuner123',
-      })
-    }
-
-    return 
-  } catch (err) {
-    throw new Error(err.message)
-  }
-}
-
-const login = async (auth, request, app) => {
-
-  await createUser()
+  const { email } = await createRandomUsers()
 
   const res = await request(app)
     .post('/api/login')
     .send({
-      email: 'tuner@pirates.sea',
-      password: 'tuner123',
+      email,
+      password: 'secret',
     })
     .expect(200)
   
   auth.token = await res.body.accessToken
-  
-  return
+  auth.user = await res.body.user
 }
-
-module.exports = login
