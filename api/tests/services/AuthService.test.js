1410c6a2915e38f42779ab29b2a523ebc4f5f017
Jefferson Pedro <jeffersonpedro89@gmail.com>
2024-05-29 00:16:01 -0300

    test: :test_tube: add: unit tests for AuthService

--- api/tests/services/AuthService.test.js
+++ api/tests/services/AuthService.test.js
@@ -1,169 +1,138 @@
-const { describe, it, expect, afterAll } = require('@jest/globals')
-const { createRandomUsers } = require('../helper/seeders.js')
+const { describe, it, expect, beforeAll } = require('@jest/globals')
+const { compare, genSalt, hash } = require('bcrypt')
+const { sign } = require('jsonwebtoken')
+const { v4: uuid } = require('uuid')
 const AuthService = require('../../services/AuthService.js')
-const authService = new AuthService()
+const Api404Error = require('../../errors/api404Error.js')
+const UnauthorizedError = require('../../errors/unauthorizedError.js')
+const InternalServerError = require('../../errors/internalServerError.js')
+const dataSource = require('../../database/models')
+const ValidationError = require('../../errors/validationError.js')
+const InvalidArgumentError = require('../../errors/invalidArgumentError.js')
 
-describe('Auth Service', () => {
+jest.mock('bcrypt')
+jest.mock('jsonwebtoken')
+jest.mock('uuid')
+jest.mock('../../database/models', () => ({
+  User: {
+    findOne: jest.fn(),
+    create: jest.fn()
+  }
+}))
 
-  afterAll(async () => {
-    // clear db
-    await authService.deleteRecord({ 
-      truncate: { cascade: true }, 
-      force: true 
+describe('AuthService', () => {
+  let authService
+
+  beforeAll(() => {
+    authService = new AuthService()
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  describe('register', () => {
+    it('should register successfully and return new user', async () => {
+      const dto = {
+        email: 'newuser@example.com',
+        password: 'password123'
+      }
+      const hashedPassword = 'hashedPassword'
+      const salt = 'salt'
+      const newUser = {
+        id: 'uuid',
+        ...dto,
+        password: hashedPassword,
+        salt
+      }
+      genSalt.mockResolvedValue(salt)
+      hash.mockResolvedValue(hashedPassword)
+      uuid.mockReturnValue('uuid')
+      dataSource.User.create.mockResolvedValue(newUser)
+
+      const result = await authService.register(dto)
+
+      expect(genSalt).toHaveBeenCalled()
+      expect(hash).toHaveBeenCalledWith('password123', 'salt')
+      expect(dataSource.User.create).toHaveBeenCalledWith({
+        id: 'uuid',
+        ...dto,
+        password: hashedPassword,
+        salt
+      }, {})
+      expect(result).toEqual(newUser)
+    })
+
+    it('should throw error if password is not provided', async () => {
+      const mockUser = { name: 'Test', email: 'test@mail.com' }
+
+      const result = authService.register(mockUser)
+
+      await expect(result).rejects.toThrow(ValidationError)
+      await expect(result).rejects.toThrowError('password field is required')
+    })
+
+    it('should throw error if request body is empty', async () => {
+      const mockUser = {}
+
+      const result = authService.register(mockUser)
+
+      await expect(result).rejects.toThrow(InvalidArgumentError)
+      await expect(result).rejects.toThrowError('empty request body')
     })
   })
 
-  describe('Testing authService.registerUser', () => {
+  describe('login', () => {
+    it('should return user and token on successful login', async () => {
+      const mockUser = {id: 1, email: 'test@test.com', password: 'password' }
+      const token = 'hashedToken'
 
-    it('should not register a user without a password', async () => {
-      const userMock = {
-        name: 'Test',
-        email: 'test@mail.com'
-      }
+      dataSource.User.findOne.mockResolvedValue(mockUser)
+      compare.mockResolvedValue(true)
+      sign.mockReturnValue(token)
 
-      const savedUser = authService.registerUser(userMock)
+      const result = await authService.login(mockUser)
 
-      await expect(savedUser).rejects.toThrowError('password field is required')
+      expect(result).toEqual({
+        user: {
+          id: mockUser.id,
+          email: mockUser.email
+        },
+        token: expect.any(String)
+      })
     })
 
-    it('should not register a user without a valid email', async () => {
-      const userMock = {
-        name: 'Test',
-        password: 'secret123'
-      }
+    it('should throw error if user not found', async () => {
+      const mockUser = { id: 1, email: 'test@test.com', password: 'password' }
+      dataSource.User.findOne.mockResolvedValue(null)
 
-      const savedUser = authService.registerUser(userMock)
+      const result = authService.login(mockUser)
 
-      await expect(savedUser).rejects.toThrowError('email field is required')
+      await expect(result).rejects.toThrow(Api404Error)
+      await expect(result).rejects.toThrowError('This user not exist.')
     })
 
-    it('should not register a user invalid email format', async () => {
-      const userMock = {
-        name: 'Test',
-        password: 'secret123',
-        email: 'test@'
-      }
+    it('should throw error if the password is invalid', async () => {
+      const mockUser = { id: 1, email: 'test@test.com', password: 'wrongPassword' }
+      dataSource.User.findOne.mockResolvedValue(mockUser)
+      compare.mockReturnValue(false)
 
-      const savedUser = authService.registerUser(userMock)
-
-      await expect(savedUser).rejects.toThrowError('invalid email format')
-    })
-
-    it('should not register a user by passing an empty value to the email field', async () => {
-      const userMock = {
-        name: 'Test',
-        password: 'secret123',
-        email: ''
-      }
-
-      const savedUser = authService.registerUser(userMock)
-
-      await expect(savedUser).rejects.toThrowError('email field cannot be empty')
-    })
-
-    it('should not register a user without a name', async () => {
-      const userMock = {
-        email: 'test@mail.com',
-        password: 'secret123'
-      }
-
-      const savedUser = authService.registerUser(userMock)
-
-      await expect(savedUser).rejects.toThrowError('name field is required')
-    })
-
-    it('should not register a user by passing an empty value to the name field', async () => {
-      const userMock = {
-        name: '',
-        password: 'secret123',
-        email: 'secret'
-      }
-
-      const savedUser = authService.registerUser(userMock)
-
-      await expect(savedUser).rejects.toThrowError('name field cannot be empty')
-    })
-
-    it('should not be possible to register a user without providing any data', async () => {
-      const userMock = {}
-
-      const savedUser = authService.registerUser(userMock)
-
-      await expect(savedUser).rejects.toThrowError('empty request body')
-    })
-    
-    it('should register a user with name, email and password', async () => {
-      const userMock = {
-        name: 'Test',
-        email: 'test4@mail.com',
-        password: 'secret123'
-      }
-
-      const savedUser = await authService.registerUser(userMock)
-
-      expect(savedUser).toEqual(
-        expect.objectContaining({
-          role: 'standard',
-          id: expect.any(String),
-          name: userMock.name,
-          email: userMock.email,
-          password: expect.any(String),
-          salt: expect.any(String),
-          updatedAt: expect.any(Date),
-          createdAt: expect.any(Date)
-        })
-      )
-    })
-  })
-
-  describe('Testing authService.login', () => {
-    let userMock
-
-    beforeEach(async () => {
-      userMock = await createRandomUsers(1, 'secret123')
-    })
-
-    it('should login a user', async () => {
-      userMock.password = 'secret123'
-
-      const loggedUser = await authService.login(userMock)
-
-      expect(loggedUser).toHaveProperty('token')
-      expect(loggedUser.token).toEqual(expect.any(String))
-      expect(loggedUser).toHaveProperty('user')
-      expect(loggedUser.user).toEqual(
-        expect.objectContaining({
-          id: userMock.id,
-          email: userMock.email
-        })
-      )
-    })
-
-    it('should try login with non-existent user', async () => {
-      userMock.password = 'secret123'
-      userMock.email = 'wrong@mail.com'
-
-      const loggedUser = authService.login(userMock)
-
-      await expect(loggedUser).rejects.toThrowError('This user not exist.')
-    })
-
-    it('should try login with an invalid password', async () => {
-      userMock.password = 'wrongPassword!'
-
-      const loggedUser = authService.login(userMock)
+      const result = authService.login(mockUser)
       
-      await expect(loggedUser).rejects.toThrowError('Invalid email or password.')
+      await expect(result).rejects.toThrow(UnauthorizedError)
+      await expect(result).rejects.toThrowError('Invalid email or password.')
     })
 
-    it('returns an error when receiving an invalid secret env', async () => {
-      userMock.password = 'secret123'
+    it('should throw error if JWT secret is not exist', async () => {
+      const mockUser = { id: 1, email: 'test@test.com', password: 'wrongPassword' }
+      dataSource.User.findOne.mockResolvedValue(mockUser)
+      compare.mockReturnValue(true)
       process.env.JWT_SECRET = ''
 
-      const result = authService.login(userMock)
+      const result = authService.login(mockUser)
 
+      await expect(result).rejects.toThrow(InternalServerError)
       await expect(result).rejects.toThrowError('Error creating a token: secret must have a value')
     })
-
   })
 })
